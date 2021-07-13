2fa3d9d42826b1a66bb9e41aca2ad9960776019d
Хорошев Алексей Алексеевич <alexwerber42@gmail.com>
2021-07-13 16:31:02 +0700

    feat: add methods in main repository

--- app/src/main/java/xaa/werber/pulttaxi/data/repository/MainRepository.kt
+++ app/src/main/java/xaa/werber/pulttaxi/data/repository/MainRepository.kt
@@ -1,7 +1,46 @@
 package xaa.werber.pulttaxi.data.repository
 
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import xaa.werber.pulttaxi.data.entity.UserInfo
 import xaa.werber.pulttaxi.data.remote.ApiService
 
 class MainRepository(private val apiService: ApiService) {
 
+    fun SMSCodeRequest(number: String) {
+        apiService.SMSCodeRequest(number).enqueue(object : Callback<String> {
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                // request is success
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                // print error
+            }
+        })
+    }
+
+    fun authorization(number: String, password: String) {
+        apiService.authorization(number, password).enqueue(object : Callback<String> {
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                // save token to shared pref
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                // print error
+            }
+        })
+    }
+
+    fun getUserInfo(token: String) {
+        apiService.getUserInfo(token).enqueue(object : Callback<UserInfo> {
+            override fun onResponse(call: Call<UserInfo>, response: Response<UserInfo>) {
+                // save user info
+            }
+
+            override fun onFailure(call: Call<UserInfo>, t: Throwable) {
+                // print error
+            }
+        })
+    }
 }
