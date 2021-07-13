90d8459b68c201804bc2abbdb64e94ae5a6f1e3b
Хорошев Алексей Алексеевич <alexwerber42@gmail.com>
2021-07-13 15:39:56 +0700

    feat: add api service

---
+++ app/src/main/java/xaa/werber/pulttaxi/data/remote/ApiService.kt
@@ -0,0 +1,26 @@
+package xaa.werber.pulttaxi.data.remote
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+    // Запрос смс кода
+    @GET("/requestSMSCodeClient")
+    fun SMSCodeRequest(
+        @Query("phone_number") number: String
+    ) : Call<String>
+
+    // Авторизация через СМС код
+    @GET("/authenticateClients")
+    fun authorization(
+        @Query("phone_number") number: String,
+        @Query("password") password: String
+    ) : Call<String>
+
+    // Получение информации о клиенте
+    @GET("/client/getInfo")
+    fun getUserInfo(
+        @Query("token") token: String
+    ) : Call<UserInfo>
+}
