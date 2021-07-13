5a35c15526328ee4aa0ed9efddd3c396a5730063
Хорошев Алексей Алексеевич <alexwerber42@gmail.com>
2021-07-13 20:28:06 +0700

    feat: add base fragment

--- app/src/main/java/xaa/werber/pulttaxi/ui/base/BaseFragment.kt
+++ app/src/main/java/xaa/werber/pulttaxi/ui/base/BaseFragment.kt
@@ -1,6 +1,21 @@
 package xaa.werber.pulttaxi.ui.base
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import dagger.android.support.AndroidSupportInjection
+import javax.inject.Inject
 
-abstract class BaseFragment: Fragment() {
+abstract class BaseFragment<V: ViewModel>: Fragment() {
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+    lateinit var viewModel: ViewModel
+
+    abstract fun getViewModel(): Class<V>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidSupportInjection.inject(this)
+        super.onCreate(savedInstanceState)
+    }
 }
