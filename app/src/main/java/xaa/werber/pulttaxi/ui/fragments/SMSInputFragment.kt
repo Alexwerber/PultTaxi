da1bd05f54a3f89adc56ebe937848e87f49b452a
Хорошев Алексей Алексеевич <alexwerber42@gmail.com>
2021-07-14 14:21:27 +0700

    feat: add input fragments

--- app/src/main/java/xaa/werber/pulttaxi/ui/fragments/SMSInputFragment.kt
+++ app/src/main/java/xaa/werber/pulttaxi/ui/fragments/SMSInputFragment.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_phone_input.*
+import kotlinx.android.synthetic.main.fragment_sms_code_input.*
 import xaa.werber.pulttaxi.R
 import xaa.werber.pulttaxi.ui.base.BaseFragment
 import xaa.werber.pulttaxi.viewmodel.MainViewModel
 
-class PhoneInputFragment: BaseFragment<MainViewModel>() {
+class SMSInputFragment: BaseFragment<MainViewModel>() {
 
     override fun getViewModel(): Class<MainViewModel> = MainViewModel::class.java
 
@@ -18,23 +18,23 @@ class PhoneInputFragment: BaseFragment<MainViewModel>() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_phone_input, container, false)
+        return inflater.inflate(R.layout.fragment_sms_code_input, container, false)
     }
 
     override fun onStart() {
         super.onStart()
 
-        inputPhone()
+        inputSMS()
     }
 
-    private fun inputPhone() {
-        confirm_button.setOnClickListener() {
-            if (phone_input.text?.equals("") == false) {
-                viewModel.inputPhone(phone_input.text)
-                activity?.let {  }
+    private fun inputSMS() {
+        sms_confirm_button.setOnClickListener() {
+            if (sms_input.text?.equals("") == true) {
+                viewModel.inputSMS(sms_input.text)
+                // show dialog
             }
-            else phone_input.error = "Неверный формат номера"
+            else sms_input.error = "Введите код"
         }
     }
+
 }
