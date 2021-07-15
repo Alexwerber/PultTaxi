383393d8fa2126840611d732fdf037b1d8d9147e
Хорошев Алексей Алексеевич <alexwerber42@gmail.com>
2021-07-15 11:21:56 +0700

    feat: pass bundle with info in dialog

---
+++ app/src/main/java/xaa/werber/pulttaxi/ui/fragments/DialogUserInfo.kt
@@ -0,0 +1,9 @@
+package xaa.werber.pulttaxi.ui.fragments
+
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
+import xaa.werber.pulttaxi.R
+
+class DialogUserInfo: DialogFragment(R.layout.dialog_user_info) {
+
+}
--- app/src/main/java/xaa/werber/pulttaxi/ui/fragments/SMSInputFragment.kt
+++ app/src/main/java/xaa/werber/pulttaxi/ui/fragments/SMSInputFragment.kt
@@ -9,13 +9,25 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.fragment_sms_code_input.*
 import xaa.werber.pulttaxi.PultTaxiApp
 import xaa.werber.pulttaxi.R
+import xaa.werber.pulttaxi.data.entity.UserInfo
 import xaa.werber.pulttaxi.error.CheckInput
+import xaa.werber.pulttaxi.utils.BundleKeys.ACTIVE_ORDER
+import xaa.werber.pulttaxi.utils.BundleKeys.BIRTH_DAY
+import xaa.werber.pulttaxi.utils.BundleKeys.CITY
+import xaa.werber.pulttaxi.utils.BundleKeys.EMAIL
+import xaa.werber.pulttaxi.utils.BundleKeys.NAME
+import xaa.werber.pulttaxi.utils.BundleKeys.ORGANIZATION_ID
+import xaa.werber.pulttaxi.utils.BundleKeys.PHONE_NUMBER
+import xaa.werber.pulttaxi.utils.BundleKeys.RATING
+import xaa.werber.pulttaxi.utils.BundleKeys.SEX
+import xaa.werber.pulttaxi.utils.BundleKeys.STATUS
 import xaa.werber.pulttaxi.viewmodel.MainViewModel
 import xaa.werber.pulttaxi.viewmodel.ViewModelFactory
 import javax.inject.Inject
@@ -36,6 +48,7 @@ class SMSInputFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         return inflater.inflate(R.layout.fragment_sms_code_input, container, false)
     }
 
@@ -46,7 +59,7 @@ class SMSInputFragment: Fragment() {
 
         viewModel.getUserInfo().observe(this, {
             if (it.phone_number != "") {
-                //show dialog
+                showDialogInfo(it)
             }
         })
 
@@ -58,6 +71,29 @@ class SMSInputFragment: Fragment() {
             if (CheckInput.checkSMSCode(sms_input.value.toString())) {
                 viewModel.getTokenFromNetwork(sms_input.value.toString())
             }
+        }
+    }
+
+    private fun showDialogInfo(userInfo: UserInfo) {
+        val myDialogFragment = DialogUserInfo()
+        val manager = activity?.supportFragmentManager
+        val bundle = bundleOf(
+                STATUS to userInfo.status,
+                PHONE_NUMBER to userInfo.phone_number,
+                NAME to userInfo.name,
+                EMAIL to userInfo.email,
+                SEX to userInfo.sex,
+                BIRTH_DAY to userInfo.birth_day,
+                CITY to userInfo.city,
+                RATING to userInfo.rating,
+                ACTIVE_ORDER to userInfo.active_order,
+                ORGANIZATION_ID to userInfo.organization_id
+        )
+
+        myDialogFragment.arguments = bundle
+
+        if (manager != null) {
+            myDialogFragment.show(manager, "myDialog")
         }
     }
 
